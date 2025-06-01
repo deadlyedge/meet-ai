0903b7f47b03929a3ab27d6d963be3700f241aca
xdream oldlu <xdream@gmail.com>
2025-06-01 12:29:41 +0800

    22: Video Call

---
+++ src/lib/stream-video.ts
@@ -0,0 +1,11 @@
+import "server-only"
+
+import { StreamClient } from "@stream-io/node-sdk"
+
+export const streamVideo = new StreamClient(
+	process.env.NEXT_PUBLIC_STREAM_VIDEO_API_KEY as string,
+	process.env.STREAM_VIDEO_API_SECRET as string,
+	{
+		timeout: 6000,
+	},
+)
